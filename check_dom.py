264a2e89521ec7c9e6374f9e33428608cbadc58b
diveshchinchavalkar45 <diveshchinchavalkar45@users.noreply.github.com>
2026-09-08 14:22:46 +0530

    Initial commit: Agro-Sphere - Smart APMC Mandi & SIH 2026 Platform

--- check_dom.py
+++
@@ -1,4 +0,0 @@
-with open('index.html', 'r', encoding='utf-8') as f:
-    for i, line in enumerate(f, 1):
-        if 'modal-bg' in line or 'class="app"' in line:
-            print(f'Line {i}: {line.strip()[:80]}')
--- export_slide.ps1
+++
@@ -1,8 +0,0 @@
-
- = New-Object -ComObject PowerPoint.Application
- = .Presentations.Open((Resolve-Path 'test_slide5.pptx').Path, [Microsoft.Office.Core.MsoTriState]::msoTrue, [Microsoft.Office.Core.MsoTriState]::msoFalse, [Microsoft.Office.Core.MsoTriState]::msoFalse)
- = .Slides.Item(5)
-.Export((Resolve-Path '.').Path + '\test_slide5.png', 'PNG', 1920, 1080)
-.Close()
-.Quit()
-Write-Output 'Exported slide 5 to test_slide5.png'
